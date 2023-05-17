d58866b2650a1c8fd1f532825f799d9f6cf7dd06
gamal sayed <104141190+gamalsayed2002@users.noreply.github.com>
2023-05-17 13:40:12 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let imgBox = document.getElementById("imgBox");
+let qrimg = document.getElementById("qrimg");
+let qrtext = document.getElementById("qrtext");
+
+function generate() {
+  if (qrtext.value.length > 0) {
+    qrimg.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrtext.value;
+    imgBox.classList.add("show-img");
+  }
+}
